0b6720d187ba60a9066074c410e50057844d6fc5
IntangibleMatter <47702559+IntangibleMatter@users.noreply.github.com>
2020-01-02 12:48:38 -0800

    Add files via upload

---
+++ Text adventure.py
@@ -0,0 +1,12 @@
+import * from time
+import * from math
+
+print("Text Adventure")
+sleep(1)
+username = input("What is your name?: ")
+charname = input("What do you want to call your character, " + username + "?: ")
+sleep(.25)
+print("We begin our journey as " + charname + " wakes up from their sleep."
+print("They don't know it yet, but something's wrong in the kingdom."
+input("Press [Enter] to get up.")
+
