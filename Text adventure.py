6b2c146c10201bcf5e9366a4b4e021d22f4c1f22
IntangibleMatter <47702559+IntangibleMatter@users.noreply.github.com>
2020-01-02 14:37:27 -0800

    Add files via upload

--- Text adventure.py
+++ Text adventure.py
@@ -1,10 +1,13 @@
+# Import stuff here
 import time
 import math
+# main body
 print("Text Adventure")
 time.sleep(1)
 username = input("What is your name?: ")
 charname = input("What do you want to call your character, " + username + "?: ")
 time.sleep(.25)
+#first room
 def startingroom():
         print("We begin our journey as " + charname + " wakes up from their sleep.")
         print("They don't know it yet, but something's wrong in the kingdom.")
@@ -12,6 +15,7 @@ def startingroom():
         print(charname + " gets up. They have a headache.")
         print(charname + " thinks that the bed is comfy, but also wants to find something to get rid of the headache")
 		startingroomchoice1()
+#first choice in first room
  def startingroomchoice1():
 		choice = input("What do they do?: ")
         if choice == "lie down" or choice == "Go to bed":
@@ -22,5 +26,5 @@ def startingroom():
 		else:
 			print("Sorry, I don't understand. Try 'lie down' or' look around'")
 			startingroomchoice1()
-
+# code runs here
 startingroom()
